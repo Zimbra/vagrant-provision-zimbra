4f471e1b6570a5523471f66fa6ac0bfec0c409c0
Phil Pearl (Lobbes) <phil@perkpartners.com>
2015-04-01 14:38:37 -0400

    - fix doc/note on fpm

--- vsetup-ubuntu.sh
+++ vsetup-ubuntu.sh
@@ -29,8 +29,8 @@ usage()
     cat <<EOF
 Usage: $prog <[-b][-d][-r]>
   environment type (choose all desired zimbra related environments):
-    -b  == build       ThirdParty FOSS (gcc,headers,libs,etc.)
-    -d  == development Full ZCS builds (ant,fpm,java,maven,...)
+    -b  == build       ThirdParty FOSS (fpm,gcc,headers,libs,etc.)
+    -d  == development Full ZCS builds (ant,java,maven,...)
     -r  == runtime     consul, mariadb, redis, memcached
 
   Note: runtime uses non-standard ZCS components (instead of
